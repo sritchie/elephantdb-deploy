82ed5f1176153e0573b234bb1b30064a5bbcf5ae
Sam Ritchie <sritchie09@gmail.com>
2012-01-11 17:27:53 -0500

    project.clj updates.

--- project.clj
+++ project.clj
@@ -5,13 +5,12 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.cli "0.1.0"]
                  [org.antlr/stringtemplate "3.2"]]
-  :dev-dependencies [[org.cloudhoist/pallet "0.6.6"]
+  :dev-dependencies [[org.slf4j/slf4j-log4j12 "1.6.4"]
+                     [org.cloudhoist/pallet "0.6.6"]
                      [org.cloudhoist/git "0.5.0"]
                      [org.cloudhoist/java "0.5.1"]
                      [org.cloudhoist/ssh-key "0.6.0"]                   
                      [org.cloudhoist/automated-admin-user "0.6.0"]
                      [org.jclouds.provider/aws-ec2 "1.2.1"]
                      [org.jclouds.provider/aws-s3 "1.2.1"]
-                     [org.jclouds.driver/jclouds-jsch "1.2.1"]
-                     [org.jclouds.driver/jclouds-log4j "1.2.1"]
-                     [log4j/log4j "1.2.14"]])
+                     [org.jclouds.driver/jclouds-jsch "1.2.1"]])
